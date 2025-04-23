7d4e85c2937388defedf20c5e1e6ef235faa3cbc
SandaRuwan 3 <102467041+SadaruwanItzMe3@users.noreply.github.com>
2025-04-23 16:18:28 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js
+
+module.exports = {
+    // TikTok API endpoint
+    tikTokApi: 'https://api.tikwm.com', // Free TikTok API
+    tikTokApiKey: '', // Optional, if the API needs a key (for now empty)
+
+    // Command Prefix
+    prefix: '.'
+};
